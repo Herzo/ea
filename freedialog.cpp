d425158a7e839b16d1ae9a3e512052cdb1aa30d7
Quantizer <Quantizer@ezymail.com>
2015-10-10 16:24:40 +1000

    Added new icon resources

--- freedialog.cpp
+++ freedialog.cpp
@@ -6,6 +6,13 @@ CFreeDialog::CFreeDialog(QWidget *parent) :
     ui(new Ui::CFreeDialog)
 {
     ui->setupUi(this);
+    createActions();
+    createTrayIcon();
+    connect(m_pTrayIcon, SIGNAL(activated(QSystemTrayIcon::ActivationReason)),
+            this, SLOT(iconActivated(QSystemTrayIcon::ActivationReason)));
+    m_pTrayIcon->show();
+    InitIconStore();
+
 }
 
 CFreeDialog::~CFreeDialog()
@@ -17,4 +24,61 @@ void CFreeDialog::on_pushButton_clicked()
 {
     QPixmap imgTick(":/images/tick.png");
     ui->imgMark->setPixmap(imgTick);
+}
+
+CFreeDialog::InitIconStore()
+{
+
+    m_IconStore.insert(QIcon(":/images/idle.svg"),pair("Idle",""));
+    m_IconStore.insert(QIcon(":/images/running.svg"),pair("Running",""));
+    m_IconStore.insert(QIcon(":/images/warning.svg"),pair("Warning",""));
+    m_IconStore.insert(QIcon(":/images/bypass.svg"),pair("Bypass",""));
+    m_IconStore.insert(QIcon(":/images/blocking.svg"),pair("Blocking",""));
+
+
+}
+void COptionsDialog::setVisible(bool visible)
+{
+    m_pMinimizeAction->setEnabled(visible);
+    m_pMaximizeAction->setEnabled(!isMaximized());
+    m_pRestoreAction->setEnabled(isMaximized() || !visible);
+    QDialog::setVisible(visible);
+}
+
+void COptionsDialog::closeEvent(QCloseEvent *event)
+{
+    if (m_pTrayIcon->isVisible()) {
+        QMessageBox::information(this, tr("Einstein's Agent"),
+                                 tr("Einstein's Agent will keep running in the "
+                                    "system tray. To terminate the program, "
+                                    "choose <b>Quit</b> in the context menu "
+                                    "of the system tray entry."));
+        hide();
+        event->ignore();
+    }
+}
+
+void COptionsDialog::setIcon(int index)
+{
+    QIcon icon = m_pIconComboBox->itemIcon(index);
+    m_pTrayIcon->setIcon(icon);
+    setWindowIcon(icon);
+
+    m_pTrayIcon->setToolTip(m_pIconComboBox->itemText(index));
+}
+
+void COptionsDialog::iconActivated(QSystemTrayIcon::ActivationReason reason)
+{
+    switch (reason) {
+    case QSystemTrayIcon::Trigger:
+    case QSystemTrayIcon::DoubleClick:
+        m_pIconComboBox->setCurrentIndex((m_pIconComboBox->currentIndex() + 1)
+                                      % m_pIconComboBox->count());
+        break;
+    case QSystemTrayIcon::MiddleClick:
+        showMessage();
+        break;
+    default:
+        ;
+    }
 }
--- freedialog.h
+++ freedialog.h
@@ -14,12 +14,29 @@ class CFreeDialog : public QDialog
 public:
     explicit CFreeDialog(QWidget *parent = 0);
     ~CFreeDialog();
+    void setVisible(bool visible);
+
+protected:
+    void closeEvent(QCloseEvent *event);
 
 private slots:
     void on_pushButton_clicked();
+    void setIcon(int index);
+    void iconActivated(QSystemTrayIcon::ActivationReason reason);
 
 private:
     Ui::CFreeDialog *ui;
+    void createActions();
+    void createTrayIcon();
+    QAction *m_pMinimizeAction;
+    QAction *m_pMaximizeAction;
+    QAction *m_pRestoreAction;
+    QAction *m_pQuitAction;
+
+    QSystemTrayIcon *m_pTrayIcon;
+    QMenu *m_pTrayIconMenu;
+    QMap<QSting, pair< icon, QString> > m_IconStore;
+    InitIconStore();
 };
 
 #endif // FREEDIALOG_H
